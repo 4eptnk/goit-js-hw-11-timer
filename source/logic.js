92804c81b56e58e5d98181ced947c10473e6144e
Sergey Mychailichenko <4eptnk@gmail.com>
2021-05-26 21:38:28 +0300

    *
    правки

--- source/logic.js
+++ source/logic.js
@@ -4,6 +4,6 @@ var minsEl = document.querySelector("[data-value='mins']");
 var secsEl = document.querySelector("[data-value='secs']");
 
 new countDownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('May 24, 2022'),
+  selector: "#timer-1",
+  targetDate: new Date("May 24, 2022"),
 });
